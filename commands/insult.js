e84371059a36d3befe100f952e2c37e1844ff14e
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2019-11-06 15:00:01 +0530

    Update insult.js

--- commands/insult.js
+++ commands/insult.js
@@ -17,9 +17,8 @@ module.exports= {
 		  }
 		  return message.channel.send("You are adding nothing to the list MF, bleach your eyes ffs its ADD");
 	  } 
-	  console.log(config.insult);
 	  if(!args[0])
-		return message.reply(config.insult[rand]);
+		return message.reply(insult[rand]);
 	  else
 		return message.channel.send(args.join(' ')+" "+insult[rand]);
 }};
