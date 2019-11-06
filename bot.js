9cc6c5f4d6a18f5899d866e4f90466f9e4b41860
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2019-11-06 14:41:54 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const http = require("http");
 const port = 3000;
 http.createServer().listen(port);
 const client = new Discord.Client();
-//g This is your client. Some people call it `bot`, some people call it `self`, 
+// This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
 const fs = require('fs');
