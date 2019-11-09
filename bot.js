00edc33360847bf10a316f6cde3eb10994eadeb3
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2019-11-09 07:34:21 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-//import datete-and-time
+//import datee-and-time
 // Load up the discord.js library discord.js 11.5.1
 const Discord = require("discord.js");
 const config = require("./config.json");  // Here we load the config.json file that contains our token and our prefix values. 
