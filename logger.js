21c42bb67f9b36f8fd578824c66bbab9f99a4042
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2019-11-17 23:30:46 +0530

    logger updated :C

--- logger.js
+++ logger.js
@@ -2,13 +2,14 @@ let fs  = require('fs');
 const d = require("date-and-time");
 const os = require("os");
 module.exports = {
-	execute(message,client,BOTchan) {
-		const redd1 = client.channels.get(`612331585622114304`);
+	execute(message,client,BOTchan,d) {
+		/*const redd1 = client.channels.get(`612331585622114304`);
 		const redd2  =  client.channels.get(`612345860373741578`);
-		const redd3  = client.channels.get(`625695412602273792`);
-		if(BOTchan !=  message.channel.name && !message.author.bot || (message.channel in (redd1, redd2, redd3))) {
+		const redd3  = client.channels.get(`625695412602273792`); 
+		console.log(message.author.bot) */
+		if(!message.author.bot) {
 			var user = message.author.username;
 			var date = "["+d.format(new Date(), 'DD/MM/YYYY HH:mm:ss')+'] @'+message.channel.name+' '+user+' : ';
-			fs.appendFileSync("./log.txt",os.EOL+date+message.content);
+			fs.appendFileSync("log.txt",os.EOL+date+message.content);
 		}
 }};
