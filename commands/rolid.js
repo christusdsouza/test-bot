3280a95535be39cae27d314eb006e03d107885a9
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2019-12-18 13:19:21 +0530

    Update rolid.js

--- commands/rolid.js
+++ commands/rolid.js
@@ -2,6 +2,11 @@ module.exports = {
 	syntax: " <role-name>",
 	description: "Grab a roles ID by mentioning its name\n EZPZ",
 	execute(message,args) {
-		let myRole = message.guild.roles.find(role => role.name === args.join(' '));
-	   return message.channel.send(myRole.id);
+		if(args){
+			console.log('k');
+			let myRole = message.guild.roles.find(role => role.name === args.join(' '));
+			return message.channel.send(myRole.id);
+		}else {
+			return (message.guild.roles.id);
+		}
 }};
