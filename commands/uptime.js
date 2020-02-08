8e650ae24f62342d40233db69b336ac09bb7e2a6
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2020-02-08 14:57:40 +0530

    uptime

--- commands/uptime.js
+++ commands/uptime.js
@@ -3,6 +3,12 @@ module.exports = {
 	execute(message,args,client) {
 		return message.channel.send(
 			'Status: '+client.status+
-			'Uptime: '+(client.uptime/3600000)
+			'\nUptime: '+uptime(client.uptime)
 			);
 }};
+function uptime(milliseconds) {
+	var seconds = ((milliseconds / 1000) % 60);
+	var minutes = ((milliseconds / (1000*60)) % 60);
+	var hours   = ((milliseconds / (1000*60*60)) % 24);
+	return hours.toFixed(0)+':'+minutes.toFixed(0)+':'+seconds.toFixed(0);
+}
