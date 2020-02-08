5ab0c0bd11ee6e15a715dcfcdb019135d7b6305f
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2020-02-08 15:10:13 +0530

    Update rc.js

--- commands/rc.js
+++ commands/rc.js
@@ -4,8 +4,14 @@ module.exports = {
 	syntax: " <role-name> <color-code>",
 	description: "Assign a fancy color to any role\nHex color-codes are appreciated",
 	execute(message,args,client) {
-		var colorx = '0x'+args[1].split("#"); 
+		if(!message.member.hasPermission(`ADMINISTRATOR`))
+			return message.reply("Sorry, you don't have permissions to use this!");
 		var role = message.guild.roles.find(role => role.name === args[0]);
+		if(role) {
+		var colorx = '0x'+args[1].split("#"); 
 		role.edit({color: colorx});
 		message.channel.send("Changed to #"+args[1]);
+		}
+		else
+			return message.reply("No such role");
 }};
