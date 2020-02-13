fa2d4e194fa7a9580f0e8576e336b0c2596043cb
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2020-02-13 13:39:35 +0530

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -10,5 +10,7 @@ function uptime(milliseconds) {
 	var seconds = ((milliseconds / 1000) % 60);
 	var minutes = ((milliseconds / (1000*60)) % 60);
 	var hours   = ((milliseconds / (1000*60*60)) % 24);
-	return hours.toFixed(0)+':'+minutes.toFixed(0)+':'+seconds.toFixed(0);
+	var days = hours / 24;
+	if (days.toFixed(0) < 0) return hours.toFixed(0)+':'+minutes.toFixed(0)+':'+seconds.toFixed(0);
+	return days.toFixed(0)+'d'+hours.toFixed(0)+'h'+minutes.toFixed(0)+'m'+seconds.toFixed(0)+'s';
 }
