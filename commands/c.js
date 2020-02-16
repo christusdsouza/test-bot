64c40f8702ab65591b6bad491b22dbfe8089665b
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2020-02-16 12:33:37 +0530

    bot clean

---
+++ commands/c.js
@@ -0,0 +1,7 @@
+module.exports= {
+	description: "Call this to clean up bot clutter",
+	async execute(message) {
+		const fetched = await message.channel.fetchMessages();
+		message.channel.bulkDelete(fetched.filter(auth => auth.author.bot === true))
+			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`)); 
+}};
