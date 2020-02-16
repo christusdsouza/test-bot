9a7a3909c1b50d3bff40fe53f984c5ee2e5fbc67
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2020-02-16 16:18:07 +0530

    Update c.js

--- commands/c.js
+++ commands/c.js
@@ -1,8 +1,9 @@
 module.exports= {
 	description: "Call this to clean up bot clutter",
 	async execute(message) {
-		const fetched = await message.channel.fetchMessages();
-		message.channel.bulkDelete(fetched.filter(auth => auth.author.bot === true))
-			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`)); 
 		message.delete();
+		const fetched = await message.channel.fetchMessages();
+		console.log(fetched.filter(auth => auth.attachments.first()));
+		message.channel.bulkDelete(fetched.filter(auth => auth.author.bot === true || auth.attachments.first()))
+			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`)); 
 }};
