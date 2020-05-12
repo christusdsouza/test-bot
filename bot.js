d35409ad8bed8a92c8261927eaa42b1632936ea7
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2020-05-12 12:31:24 +0530

    Update bot.js
    weird, how did i miss one.. smh

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ for (const file of commandFiles) {
 
 client.on("ready", () => {
     // This event will run if the bot starts, and logs in, successfully.
-    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+    console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
     // Example of changing the bot's playing game to something useful. `client.user` is what the
     // docs refer to as the "ClientUser".
     client.user.setPresence({
