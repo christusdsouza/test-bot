a9e8d510efb08572e483414ff363984782e00e9a
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2020-05-12 13:11:37 +0530

    Update pingFS.js
    removed uncessary client

--- commands/pingFS.js
+++ commands/pingFS.js
@@ -7,9 +7,8 @@ module.exports = {
 	/***
 	 * @params Collection message
 	 * @params Array args; args[0]: GuildMember member, args[1]: int count
-	 * @params this.Client client
 	 **/
-	async execute(message, args, client) {
+	async execute(message, args) {
 		try {
 			if (!message.member.hasPermission(`MANAGE_MESSAGES`)) {
 				if (!lastCmdTime) lastCmdTime = message.createdTimestamp;
@@ -19,7 +18,7 @@ module.exports = {
 						.then(msg => msg.delete({timeout:3000}));
 			}
 			if (args.length < 2) return message.reply("You missed some stuff").then(msg => msg.delete({timeout:10000}));
-			let chan = client.channels.cache.find(chan => chan.id === `647162352797745172`);
+			let chan = message.guild.channels.cache.find(chan => chan.id === `647162352797745172`);
 			let member = message.mentions.members.first() || message.guild.members.cache.find((member) => {
 				if (member.user.username.toUpperCase() === args[0].toUpperCase()) return true;
 				else if (member.nickname != null && member.nickname.toUpperCase() === args[0].toUpperCase()) return true;
