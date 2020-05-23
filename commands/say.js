17ea5241febcddec8edbc6a64a917d3a2546e5ec
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2020-05-23 15:56:27 +0530

    instant delete

--- commands/say.js
+++ commands/say.js
@@ -16,7 +16,7 @@ module.exports = {
         // To get the "message" itself we join the `args` back into a string with spaces: 
         const sayMessage = args.join(" ");
         // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-        message.delete({timeout:10})
+        message.delete()
             .catch(O_o => { });
         // And we get the bot to say the thing: 
         message.channel.send(sayMessage);
